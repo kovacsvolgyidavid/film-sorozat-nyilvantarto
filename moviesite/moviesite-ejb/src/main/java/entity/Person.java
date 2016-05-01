cf6fea01ad3ebd941ad14d8ad2fd8ea53e08e97f
David Kovacsvolgyi <kovacsvolgyi.david@gmail.com>
2016-05-01 14:12:03 +0200

    Update person + user entity

--- moviesite/moviesite-ejb/src/main/java/entity/Person.java
+++ moviesite/moviesite-ejb/src/main/java/entity/Person.java
@@ -1,5 +1,9 @@
 package entity;
 
+import enums.Sex;
+import java.util.Date;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 /**
@@ -8,5 +12,55 @@ import javax.persistence.MappedSuperclass;
  */
 @MappedSuperclass
 public class Person {
+    @Id@GeneratedValue
+    private Long id;
+    private String name;
+    private Date birthDay;
+    private String imageURL;
+    private Sex sex;
+
+    public Person() {
+        //FOR MAPPING REASONS
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
     
 }
--- moviesite/moviesite-ejb/src/main/java/entity/User.java
+++ moviesite/moviesite-ejb/src/main/java/entity/User.java
@@ -1,9 +1,12 @@
 package entity;
 
+import javax.persistence.Entity;
+
 /**
  *
  * @author David Kovacsvolgyi <kovacsvolgyi.david@gmail.com>
  */
+@Entity
 public class User {
     
 }
