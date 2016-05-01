f61ae6aa8567797455eabdb107642f2f6a0bfcd8
David Kovacsvolgyi <kovacsvolgyi.david@gmail.com>
2016-05-01 12:57:18 +0200

    Update entities

--- moviesite/moviesite-ejb/src/main/java/entity/Person.java
+++ moviesite/moviesite-ejb/src/main/java/entity/Person.java
@@ -1,9 +1,12 @@
 package entity;
 
+import javax.persistence.MappedSuperclass;
+
 /**
  *
  * @author David Kovacsvolgyi <kovacsvolgyi.david@gmail.com>
  */
+@MappedSuperclass
 public class Person {
     
 }
