27ac2b425d4e3db85f0b39c8ebecc99763b0e426
David Kovacsvolgyi <kovacsvolgyi.david@gmail.com>
2016-05-16 12:45:38 +0200

    Start of seriessite

---
+++ seriessite/seriessite-ejb/src/main/java/xyz/codingmentor/entity/Comment.java
@@ -0,0 +1,80 @@
+package xyz.codingmentor.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Comment implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String content;
+
+    @Column(name = "DATE_OF_COMMENT")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateOfComment;
+    
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn(name = "SHOW_ID")
+    private Movie show;
+    
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn(name = "USERNAME"/*, nullable = false*/)
+    private Users users;
+   
+    public Comment() {
+        //it is bean
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDateOfComment() {
+        return dateOfComment;
+    }
+
+    public void setDateOfComment(Date dateOfComment) {
+        this.dateOfComment = dateOfComment;
+    }
+
+    public Movie getShow() {
+        return show;
+    }
+
+    public void setShow(Movie show) {
+        this.show = show;
+    }
+
+    public Users getUsers() {
+        return users;
+    }
+
+    public void setUsers(Users users) {
+        this.users = users;
+    }
+    }
+
---
+++ seriessite/seriessite-ejb/src/main/java/xyz/codingmentor/entity/Users.java
@@ -0,0 +1,140 @@
+package xyz.codingmentor.entity;
+
+import xyz.codingmentor.enums.Groups;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import xyz.codingmentor.constraint.NameConstraint;
+import xyz.codingmentor.constraint.UsernameConstraint;
+
+@Entity
+@Table(name = "USERS")
+@NamedQuery(name = "findUserByUsername", 
+        query = "SELECT u FROM Users u WHERE u.username = :username")
+public class Users implements Serializable {
+    @Id
+    @Size(min = 1, message = "This field has to be filled.")
+    @UsernameConstraint(message = "Wrong username format.")
+    private String username;
+    
+    //@Size(min = 1, message = "This field has to be filled.")
+    //@PasswordConstraint(message = "Wrong password format.")
+    private String password;
+    
+    @Column(name="MOVIE_PER_PAGE")
+    private Integer moviePerPage;
+    
+    @CollectionTable(name="groups")
+    @ElementCollection
+    @Enumerated(EnumType.STRING)
+    private Set<Groups> groups=new HashSet<>();
+     @Size(min = 1, message = "This field has to be filled.")
+    @NameConstraint(message = "Wrong name format.")
+    private String name;
+    
+    private String sex;
+    
+    @Column(name="DATE_OF_BIRTH")
+    @Temporal(TemporalType.DATE)
+    @NotNull(message = "This field has to be filled.")
+    private Date dateOfBirth;
+    
+    @Column(name="PATH_OF_PHOTO")
+    private String pathOfPhoto;
+    
+@OneToMany (mappedBy = "users")
+private List<Comment> comments;
+    
+    public Users() {
+        //Empty
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getMoviePerPage() {
+        return moviePerPage;
+    }
+
+    public void setMoviePerPage(Integer moviePerPage) {
+        this.moviePerPage = moviePerPage;
+    }
+    
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public Set<Groups> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(Set<Groups> groups) {
+        this.groups = groups;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPathOfPhoto() {
+        return pathOfPhoto;
+    }
+
+    public void setPathOfPhoto(String pathOfPhoto) {
+        this.pathOfPhoto = pathOfPhoto;
+    }
+    
+}
