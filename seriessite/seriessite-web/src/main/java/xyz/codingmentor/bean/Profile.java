f5d284318279adee38c2b3d363ab3964e4858ee5
József Pránder <pranderj@gmail.com>
2016-05-26 17:58:25 +0200

    new external template

--- seriessite/seriessite-web/src/main/java/xyz/codingmentor/bean/Profile.java
+++ seriessite/seriessite-web/src/main/java/xyz/codingmentor/bean/Profile.java
@@ -43,7 +43,7 @@ public class Profile implements Serializable {
 
     @Inject
     private DatabaseQuery databaseQuery;
-    
+
     @Inject
     private Registration registration;
 
@@ -59,32 +59,22 @@ public class Profile implements Serializable {
     public void init() {
         sexes[0] = Sex.MALE;
         sexes[1] = Sex.FEMALE;
-        
+
         userDTO = new UserDTO();
         userDTO.setUser(new User());
-        //modifiedUser = new Users();
     }
 
-    public String getUserProfile(User user) {
-        //originalUser = user;
-        //resetUser(modifiedUser, originalUser);
-        uploadedFile = null;
-        this.user = user; //resetelj a copy metódussal
-//        userDTO.setUser(user);
-        return "/profile?faces-redirect=true";  
-    }
-    
     public String getMyProfile() {
         user = entityFacade.read(User.class, Usermanagement.getUsername());
 //        userDTO.setUser(user);
         return "/user/profile?faces-redirect=true";
     }
-    
-    public String getUserProfile2(User user) {
+
+    public String getUserProfile(User user) {
         uploadedFile = null;
         this.user = user;
 //        userDTO.setUser(user);
-        
+
         FacesContext context = FacesContext.getCurrentInstance();
         Map<String, String> params = context.getExternalContext().getRequestParameterMap();
         String id = params.get("userId");
@@ -93,32 +83,37 @@ public class Profile implements Serializable {
         return "/user/profile.xhtml?faces-redirect=true";
 //        return "actorEdit.xhtml/?id="+1+",faces-redirect=true";
     }
-    
+
     public void saveUserData() {
         uploadPicture();
         entityFacade.update(user);
 
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("The modification is successful."));
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("The modification was successful."));
     }
 
     public void saveUserPassword() {
-        //if(hashPassword(oldPassword).equals(entityFacade.read(Users.class, user.getUsername()).getPassword())){
         String oldPasswordFromTable = entityFacade.read(User.class, user.getUsername()).getPassword();
-        
-        if(hashPassword(oldPassword).equals(oldPasswordFromTable)){   
+
+        if (hashPassword(oldPassword).equals(oldPasswordFromTable)) {
             user.setPassword(hashPassword(userDTO.getPassword()));
             entityFacade.update(user);
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Succes!", "The password has been changed."));
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("The password has been changed."));
         } else {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "The old password is incorrect."));
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "The old password is incorrect.", "Error!"));
         }
     }
-    
+
     public void onTabChange(TabChangeEvent event) {
         try {
             uploadedFile = null;
-            //user = entityFacade.read(Users.class, user.getUsername());
-            image = new DefaultStreamedContent(new FileInputStream(user.getPathOfPhoto()));
+
+            if (user.getPathOfPhoto().equals("user.jpg")) {
+                ClassLoader classLoader = getClass().getClassLoader();
+                File noPicture = new File(classLoader.getResource("/user.jpg").getFile());
+                image = new DefaultStreamedContent(new FileInputStream(noPicture));
+            } else {
+                image = new DefaultStreamedContent(new FileInputStream(user.getPathOfPhoto()));
+            }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Profile.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -126,7 +121,7 @@ public class Profile implements Serializable {
 
     public void uploadPicture() {
         createDirectory();
-        if(uploadedFile != null){
+        if (uploadedFile != null) {
             try {
                 InputStream inputstream = uploadedFile.getInputstream();
                 String fullFileName = uploadedFile.getFileName();
@@ -167,7 +162,7 @@ public class Profile implements Serializable {
     public void setUserDTO(UserDTO userDTO) {
         this.userDTO = userDTO;
     }
-    
+
     public String getOldPassword() {
         return oldPassword;
     }
@@ -175,17 +170,17 @@ public class Profile implements Serializable {
     public void setOldPassword(String oldPassword) {
         this.oldPassword = oldPassword;
     }
-    
+
     public StreamedContent getImage() {
         try {
             if (uploadedFile == null) {
-                if(user.getPathOfPhoto().equals("user.jpg")){
+                if (user.getPathOfPhoto().equals("user.jpg")) {
                     ClassLoader classLoader = getClass().getClassLoader();
                     File noPicture = new File(classLoader.getResource("/user.jpg").getFile());
-                    image = new DefaultStreamedContent(new FileInputStream(noPicture));                    
-                } else{
+                    image = new DefaultStreamedContent(new FileInputStream(noPicture));
+                } else {
                     image = new DefaultStreamedContent(new FileInputStream(user.getPathOfPhoto()));
-                }    
+                }
             } else {
                 image = new DefaultStreamedContent(uploadedFile.getInputstream());
             }
@@ -212,10 +207,10 @@ public class Profile implements Serializable {
     public Enum[] getSexes() {
         return sexes;
     }
-    
-    public String hashPassword(String password){
+
+    public String hashPassword(String password) {
         String hashedPassword = null;
-        
+
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             String text = password;
@@ -224,10 +219,9 @@ public class Profile implements Serializable {
             BigInteger bigInt = new BigInteger(1, digest);
             hashedPassword = bigInt.toString(16);
 
-            
         } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
             Logger.getLogger(Profile.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return hashedPassword;     
+        return hashedPassword;
     }
 }
